d091c7f34aeca0f85a827636c15edc1cddd627a9
VAST-THE-DOGE <145150405+VAST-THE-DOGE@users.noreply.github.com>
2023-09-19 16:36:01 -0500

    Update ProjectTestB.py

--- ProjectTestB.py
+++ ProjectTestB.py
@@ -1,5 +1,13 @@
 #recieve
+from time import time
 import RPi.GPIO as GPIO
 SENSOR = 0
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+while True:
+  if GPIO.input(SENSOR) == True:
+    StartTime = time()
+    while GPIO.input(SENSOR) == True:
+      pass
+    EndTime = time()
+    TimeOn = EndTime - StartTime
