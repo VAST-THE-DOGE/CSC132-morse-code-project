067a3c016cf789c7ab45fc16549abbf524bd9bbe
VAST-THE-DOGE <145150405+VAST-THE-DOGE@users.noreply.github.com>
2023-09-19 17:38:54 -0500

    Update ProjectTestB.py

--- ProjectTestB.py
+++ ProjectTestB.py
@@ -2,13 +2,18 @@
 from time import time
 import RPi.GPIO as GPIO
 SENSOR = 0
+TimeToTest = 20
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-while True:
-  if GPIO.input(SENSOR) == True:
-    StartTime = time()
-    while GPIO.input(SENSOR) == True:
-      pass
-    EndTime = time()
-    TimeOn = EndTime - StartTime
-    print(TimeOn)
+def find(SENSOR, TimeToTest):
+    i = 0
+    while i < TimeToTest:
+        if GPIO.input(SENSOR) == True:
+            StartTime = time()
+            while GPIO.input(SENSOR) == True:
+                pass
+            EndTime = time()
+            TimeOn = EndTime - StartTime
+        i += 1
+
+print(find(SENSOR, TimeToTest))
