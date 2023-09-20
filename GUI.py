0d7ba5672c70c202be20aea3ad0bc6cc0728d6e0
VAST-THE-DOGE <145150405+VAST-THE-DOGE@users.noreply.github.com>
2023-09-20 12:01:09 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -13,15 +13,16 @@ class MainGUI(Frame):
     def setupGUI(self):
         for row in range(9): #setup the rows and columns
             Grid.rowconfigure(self, row, weight=1)
-        for col in range(7):
+        for col in range(8):
             Grid.columnconfigure(self, col, weight=1)
+
         self.pack(fill=BOTH, expand=1)
 
         #exit button
-        self.eb = Button(self, anchor=N, text="X", bg="red", font=("TexGyreAdventor", 45))
+        self.eb = Button(self, anchor=N+E, text="X", bg="red", font=("TexGyreAdventor", 45))
         self.eb.grid(row=0, column=9, sticky=E+W+N+S)
         #translate button
-        self.tb = Button(self, anchor=N, text="Translate", bg="grey", font=("TexGyreAdventor", 45))
+        self.tb = Button(self, anchor=N, text="Translate", bg="grey", font=("TexGyreAdventor", 45), command=lambda: self.send(self.uiw.get()))
         self.tb.grid(row=1, column=8, sticky=E+W+N+S)
         #record button
         self.rb = Button(self, anchor=N, text="Record", bg="red", font=("TexGyreAdventor", 45))
@@ -30,36 +31,43 @@ class MainGUI(Frame):
         self.uiw = Entry(self, bg="white", font=("TexGyreAdventor", 45))
         self.uiw.grid(row=1, column=1, columnspan=6, sticky=E+W+N+S)
         #output window
-        self.ow = Label(self, text="W.I.P.", anchor=N, bg="white", height=1, font=("TexGyreAdventor", 30))
+        self.ow = Label(self, text="W.I.P.", anchor=W, bg="white", height=1, font=("TexGyreAdventor", 30))
         self.ow.grid(row=3, column=1, rowspan=2, columnspan=5, sticky=E+W+N+S)
 
         ###############
         #quick buttons#
         ###############
 
-        self.qb1 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb1.grid(row=6, column=1, sticky=E+W+N+S)
-        self.qb2 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb2.grid(row=6, column=2, sticky=E+W+N+S)
-        self.qb3 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb3.grid(row=6, column=3, sticky=E+W+N+S)
-        self.qb4 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb4.grid(row=6, column=4, sticky=E+W+N+S)
-        self.qb5 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb5.grid(row=6, column=5, sticky=E+W+N+S)
-        self.qb6 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb6.grid(row=6, column=6, sticky=E+W+N+S)
-        self.qb7 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb7.grid(row=6, column=7, sticky=E+W+N+S)
-        self.qb8 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
-        self.qb8.grid(row=6, column=8, sticky=E+W+N+S)
+        self.qb1 = Button(self, anchor=N, text="SOS", bg="grey", font=("TexGyreAdventor", 45), command=lambda: self.send("SOS"))
+        self.qb1.grid(row=7, column=1, sticky=E+W+N+S)
+        self.qb2 = Button(self, anchor=N, text="YES", bg="grey", font=("TexGyreAdventor", 45), command=lambda: self.send("YES"))
+        self.qb2.grid(row=7, column=2, sticky=E+W+N+S)
+        self.qb3 = Button(self, anchor=N, text="NO", bg="grey", font=("TexGyreAdventor", 45), command=lambda: self.send("NO"))
+        self.qb3.grid(row=7, column=3, sticky=E+W+N+S)
+        self.qb4 = Button(self, anchor=N, text="HELLO", bg="grey", font=("TexGyreAdventor", 45), command=lambda: self.send("HELLO"))
+        self.qb4.grid(row=7, column=4, sticky=E+W+N+S)
+        self.qb5 = Button(self, anchor=N, text="GOODBYE", bg="grey", font=("TexGyreAdventor", 45), command=lambda: self.send("GOODBYE"))
+        self.qb5.grid(row=7, column=5, sticky=E+W+N+S)
+        #self.qb6 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
+        #self.qb6.grid(row=6, column=6, sticky=E+W+N+S)
+        #self.qb7 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
+        #self.qb7.grid(row=6, column=7, sticky=E+W+N+S)
+        #self.qb8 = Button(self, anchor=N, text="-", bg="grey", font=("TexGyreAdventor", 45))
+        #self.qb8.grid(row=6, column=8, sticky=E+W+N+S)
 
         #details
-        #self.bg1 = Label(self, bg="black")
+        #self.bg1 = Label(self, bg="grey")
         #self.bg1.grid(row=1, column=9, rowspan=8, sticky=E+W+N+S)
-        #self.bg2 = Label(self, bg="black")
+        #self.bg2 = Label(self, bg="grey")
         #self.bg2.grid(row=0, column=0, columnspan=9, sticky=E+W+N+S)
+        self.bg3 = Label(self, bg="lightgrey")
+        self.bg3.grid(row=1, column=7, sticky=E+W+N+S)
+
+    def send(self, sendInfo):
+        print(sendInfo)
+        self.ow.config(text=sendInfo)
 
 window = Tk()
 p = MainGUI(window)
 window.mainloop()
+
