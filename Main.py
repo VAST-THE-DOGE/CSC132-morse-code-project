ebe9a797bf4f9f1d06715dd44085d2b1b930ff54
VAST-THE-DOGE <145150405+VAST-THE-DOGE@users.noreply.github.com>
2023-09-28 16:19:45 -0500

    Huge Update to Main.py
    I added about 100 lines of code! I created a way to record that needs to be tested and I improved the speed of running a tiny bit by removing the debug printout that was lagging the "ENGtoMC" function.

--- Main.py
+++ Main.py
@@ -6,9 +6,11 @@ import sys
 import os
 #Settings
 FULLSCREEN = True
-DEBUG = True
+DEBUG = False
 SHUTDOWN = False
 SEND_SPEED = 0.1
+RECORD_IDLE_TIME = 10
+RECORD_OFF_TIME = 0.001
 #get the system color
 #wip
 COLOR = True #True = light mode; False = dark mode
@@ -59,8 +61,6 @@ class MainGUI(Frame):
         self.update_idletasks() # update the window state
         width = self.winfo_screenwidth() # get the window width
         height = self.winfo_screenheight() # get the window height
-        print(width)
-        print(height)
         width //= 8
         height //= 9
 
@@ -72,13 +72,13 @@ class MainGUI(Frame):
         self.pack(fill=BOTH, expand=1)
         
         #exit button
-        self.eb = Button(self, text="X", bg="red", command=lambda: end(GPIO_ACTIVE), font=("TexGyreAdventor", 30))
+        self.eb = Button(self, text="X", bg="red", command=lambda: end(), font=("TexGyreAdventor", 30))
         self.eb.grid(row=0*height, column=9*width, sticky=E+W+N+S, padx=5, pady=5)
         #translate button
         self.tb = Button(self,  text="Send", bg="green", font=("TexGyreAdventor", 45), command=lambda: self.send(self.uiw.get()))
         self.tb.grid(row=1*height, column=8*width, sticky=E+W+N+S, padx=5, pady=5)
         #record button
-        self.rb = Button(self,  text="Record", bg="orange", font=("TexGyreAdventor", 45))
+        self.rb = Button(self,  text="Record", bg="orange", command=lambda: self.record(), font=("TexGyreAdventor", 45))
         self.rb.grid(row=3*height, column=7*width, rowspan=2*height, columnspan=2*width, sticky=E+W+N+S, padx=5, pady=5)
         #user input window
         self.uiw = Entry(self, bg="white", font=("TexGyreAdventor", 45))
@@ -142,9 +142,102 @@ class MainGUI(Frame):
             self.ow.config(text=("translating: " + sendInfo + "\n sending: " + newSendInfo + "\n GPIO IS OFF! THIS WILL NOT SEND!!!"))
         if DEBUG: print("----END----"), print("----send----")
 
+    def record(self):
+        if DEBUG: print("----record----"), print("----START----")
+        if GPIO_ACTIVE:
+            if DEBUG: print("---GPIO_IS_ON---")
+            string = ""
+            StartTime = time()
+            #wait until there is IR light detected
+            while GPIO.input(SENSOR) == False:
+                if time() - StartTime < RECORD_IDLE_TIME: self.ow.config(text=("IDLE TIME: {}/{}\nWAITING TO START".format(time() - StartTime, RECORD_IDLE_TIME)))
+                else: return "No Recording"
+            #start recording
+            while True:
+                #get ON/OFF time
+                if GPIO.input(SENSOR) == True:
+                    Time = self.recordON(string)
+                else:
+                    Time = self.recordOFF(string)
+                #if time == idle time: return the string and end recording
+                if Time == -RECORD_IDLE_TIME:
+                    return string
+                #if this is the first time, setup some stuff
+                if string == "":
+                    CurrentTime = Time
+                    string = "?"
+                    currentSymbol = "?"
+                    TimeNotFound = True
+                #else: find the time difference and find the next symbol
+                else:
+                    TimeDif = CurrentTime / Time
+                    #1#“.” To something: “-” 0.33333... “/” -0.33333... “//” -0.1328571429 
+                    #3#“-” to something:“//” -0.4285714286 “/” -1 “.” 3 
+                    #-3#“/” to something: “-” -1 “.” -3 
+                    #-7#“//” to something: “-” -2.3333333... “.” -7 
+                    #test
+                    if currentSymbol == "/ " :
+                        if TimeDif in range(-2, 1):
+                            currentSymbol = "- "
+                        elif TimeDif in range(-4, -2):
+                            currentSymbol = ". "
+                        else: #error
+                            pass
+                    elif currentSymbol == "^ / ":
+                        if TimeDif in range(-4.666666666666667, 1):
+                            currentSymbol = "- "
+                        elif TimeDif in range(-10, -4.666666666666667):
+                            currentSymbol = ". "
+                        else: #error
+                            pass
+                    else:
+                        if TimeDif in range(1.6666666666666667, 5): #-to. #3 #
+                            if TimeNotFound: string = "- "; TimeNotFound = False
+                            currentSymbol = ". "
+                        elif TimeDif in range(0.09523809523809523, 1.6666666666666667): #.to- #0.3333 #
+                            if TimeNotFound: string = ". "; TimeNotFound = False
+                            currentSymbol = "- "
+                        elif TimeDif in range(-0.23809523809523808, 0.09523809523809523): #.to// #-0.1328 #
+                            if TimeNotFound: string = ". "; TimeNotFound = False
+                            currentSymbol = "^ / "
+                        elif TimeDif in range(-0.38095238095238093, -0.23809523809523808): #.to/ #-0.3333 #
+                            if TimeNotFound: string = ". "; TimeNotFound = False
+                            currentSymbol = "/ "
+                        elif TimeDif in range(-0.7142857142857143, -0.38095238095238093): #-to// #-0.4285 #
+                            if TimeNotFound: string = "- "; TimeNotFound = False
+                            currentSymbol = "^ / "
+                        elif TimeDif in range(-2, -0.7142857142857143): #-to/ #-1 #
+                            if TimeNotFound: string = "- "; TimeNotFound = False
+                            currentSymbol = "/ "
+                        else: #error
+                            pass
+                    string += currentSymbol
+                            
+        else:
+            if DEBUG: print("---!!!GPIO_IS_OFF!!!---")
+            self.ow.config(text=("GPIO IS OFF! RECORDING IS OFFLINE!!!"))
+
+        if DEBUG: print("----END----"), print("----record----")
+
+    def recordON(self, string):
+        StartTime = time()
+        while True:
+            self.ow.config(text=("RECORDING: {}\nCURRENT STRING: {}".format(time() - StartTime, string)))
+            if GPIO.input(SENSOR) == False:
+                startOFF = time()
+                while GPIO.input(SENSOR) == False:
+                    if time() - startOFF > RECORD_OFF_TIME: return (time() - StartTime)
+
+    def recordOFF(self, string):
+        StartTime = time()
+        while GPIO.input(SENSOR) == False:
+            if time() - StartTime < RECORD_IDLE_TIME: self.ow.config(text=("IDLE TIME: {}/{}\nCURRENT STRING: {}".format(time() - StartTime, RECORD_IDLE_TIME, string)))
+            else: return -RECORD_IDLE_TIME
+        return -(time() - StartTime)
+
+
 def ENGtoMC(string):
     if DEBUG: print("--ENGtoMC--"), print("--START--")
-    backupString = string #create a backup
     string = string.lower() #set all to lowercase to keep it the same.
     #remove symbols that will create errors, the rest will be ignored.
     string = string.replace("^", "") #seperator for words!
@@ -152,12 +245,8 @@ def ENGtoMC(string):
     string = string.replace(".", "") #part of morse code!
     string = string.replace("-", "") #part of morse code!
     #end of removing
-    if string == "": return backupString #this is if the user types morse code already
-    else:
-        del backupString
-        for index in range(0 ,len(N_ALPHABET)): #replace the letters.
-            if DEBUG: print("replace '{}' with '{}'".format(N_ALPHABET[index], (MC_ALPHABET[index] + "/ ")))
-            string = string.replace(N_ALPHABET[index], (MC_ALPHABET[index] + "/ "))
+    for index in range(0 ,len(N_ALPHABET)): #replace the letters.
+        string = string.replace(N_ALPHABET[index], (MC_ALPHABET[index] + "/ "))
     if DEBUG: print("--END--"), print("--ENGtoMC--")
     return string
 
@@ -172,10 +261,7 @@ def MCtoENG(string):
     if DEBUG: print("--END--"), print("--MCtoENG--")
     return string
 
-def Record():
-    if DEBUG: print("WIP")
-
-def end(GPIO_ACTIVE):
+def end():
     window.attributes("-fullscreen", False)
     if GPIO_ACTIVE and SHUTDOWN: os.system ("sudo shutdown -h now")
     #sys.exit(0)
